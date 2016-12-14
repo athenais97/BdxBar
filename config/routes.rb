bcb871db46f06bbbe3ea4fb344c21b64ac95f0b0
user <you@example.com>
2016-12-14 10:06:26 +0100

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :bars
-  root :to => "bars#index"
+  root "bars#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
